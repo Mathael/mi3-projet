a30915008917ff1ccc88637ee151ff6d88a6f59f
Mathael <LebocPhilippe@live.be>
2016-10-08 18:54:49 +0200

    refactor double quote to simple quote

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -7,7 +7,7 @@
 	$imgId = $img->getId();
 
 	// Regarde si une taille pour l'image est connue
-	$size = isset($_GET["size"]) && is_integer($_GET["size"]) ? $size = $_GET['size'] : 480;
+	$size = isset($_GET['size']) && is_integer($_GET['size']) ? $size = $_GET['size'] : 480;
     $zoom = isset($_GET['zoom']) && is_numeric($_GET['zoom']) ? $zoom = $_GET['zoom'] : 1;
 
     if($zoom != 1) $size *= $zoom;
@@ -18,7 +18,7 @@
 	$nextImgId = $imageDAO->getNextImage($img)->getId();
 	$imgUrl = $img->getURL();
 
-	$menu['First']='?action=pictures&imgId='.$firstImgId.'&size=$size';
+	$menu['First']='?action=pictures&imgId='.$firstImgId.'&size='.$size;
 	$menu['Random']='?action=pictures&imgId='.$imageDAO->getRandomImage();
 	$menu['More']='?action=pictures&imgId='.$imgId;
 	$menu['Zoom +']='?action=pictures&zoom='.($zoom+0.25).'&imgId='.$imgId.'&size='.$size;
