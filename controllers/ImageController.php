e91b8f292caeaed05cf905ba8f072598ebe12e19
Mathael <LebocPhilippe@live.be>
2016-10-08 18:30:47 +0200

    fix(zoom): enabled zoom in and zoom out

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -8,6 +8,9 @@
 
 	// Regarde si une taille pour l'image est connue
 	$size = isset($_GET["size"]) && is_integer($_GET["size"]) ? $size = $_GET['size'] : 480;
+    $zoom = isset($_GET['zoom']) && is_numeric($_GET['zoom']) ? $zoom = $_GET['zoom'] : 1;
+
+    if($zoom != 1) $size *= $zoom;
 
 	// Crée les variables nécessaires à la vue [image.html]
 	$firstImgId = $imageDAO->getFirstImage()->getId();
@@ -18,8 +21,8 @@
 	$menu['First']='?action=pictures&imgId='.$firstImgId.'&size=$size';
 	$menu['Random']='?action=pictures&imgId='.$imageDAO->getRandomImage();
 	$menu['More']='?action=pictures&imgId='.$imgId;
-	$menu['Zoom +']='?action=pictures&zoom=1.25&imgId='.$imgId.'&size='.$size;
-	$menu['Zoom -']='?action=pictures&zoom=0.75&imgId='.$imgId.'&size='.$size;
+	$menu['Zoom +']='?action=pictures&zoom='.($zoom+0.25).'&imgId='.$imgId.'&size='.$size;
+	$menu['Zoom -']='?action=pictures&zoom='.($zoom-0.25).'&imgId='.$imgId.'&size='.$size;
 
 	require_once(VIEW_DIR.'commons/menu.html');
 	require_once(VIEW_DIR.'image.html');
