56ed49e1277e158615ee43ffebb15ff0be3acff7
Mathael <LebocPhilippe@live.be>
2016-10-20 22:48:12 +0200

    add(Util): Utilities class

---
+++ utils/Util.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author LEBOC Philippe.
+ * Date: 20/10/2016
+ * Time: 11:10
+ */
+class Util
+{
+    /**
+     * Fonction permettant de vérifier qu'une valeur existe bien dans un tableau
+     *  si oui : retourne la valeur présente dans le tableau
+     *  si non : retourne la valeur par défaut
+     * @param $tab
+     * @param $key
+     * @param $defaultValue
+     * @return mixed
+     */
+    public static function getValue($tab, $key, $defaultValue) {
+        if(array_key_exists($key, $tab))
+            return $tab[$key];
+        return $defaultValue;
+    }
+}
