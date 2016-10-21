2c9aba49ae234e8c79b263b39a32779ad0e2d3de
Mathael <LebocPhilippe@live.be>
2016-10-21 19:46:00 +0200

    fix(controller): add missing menu inclusion

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -6,6 +6,7 @@ if(!defined("FRONT_CONTROLLER"))
 
 final class IndexController implements DefaultController {
     public static function indexAction() {
+        require_once VIEW_DIR.'commons/menu.html';
         require_once VIEW_DIR.'index.html';
     }
 }
