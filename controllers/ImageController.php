1d21f29ccb81e34f0a410fc83dded3f8ae1b0212
Mathael <LebocPhilippe@live.be>
2016-10-26 23:28:40 +0200

    minor bug fixes

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -80,15 +80,15 @@ final class ImageController implements DefaultController {
     private static function buildMenu($params = []) {
         $size = Util::getValue($params, 'size', 1);
         $display = Util::getValue($params, 'display', 1);
-        $imgId = Util::getValue($params, 'id', 1);
+        $imgId = Util::getValue($params, 'id', ImageDAO::getFirstImage()->getId());
 
         return [
-            'first' => '?page=image&size='.$size.'&display='.$display,
-            'random' => '?page=image&action=random&&id='.$imgId.'size='.$size.'&display='.$display,
+            'first' => '?page=image&id='.$imgId.'&size='.$size.'&display='.$display,
+            'random' => '?page=image&action=random&id='.$imgId.'size='.$size.'&display='.$display,
             'more' => '?page=image&action=more&id='.$imgId.'&size='.$size.'&display='.$display * 2,
             'less' => '?page=image&action=less&id='.$imgId.'&size='.$size.'&display='.max(1, $display / 2),
-            'zoom +' => '?page=image&id='.$imgId.'&size='.($size*1.25).'&display='.$display,
-            'zoom -' => '?page=image&id='.$imgId.'&size='.($size*0.75).'&display='.$display
+            'zoom +' => '?page=image&id='.$imgId.'&size='.($size+0.25).'&display='.$display,
+            'zoom -' => '?page=image&id='.$imgId.'&size='.($size-.25).'&display='.$display
         ];
     }
 }
